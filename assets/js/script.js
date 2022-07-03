866a666dc0e0d199f6b338a14d3e71d8af5cb076
Parker McKillop <mckilpar000@hotmail.com>
2022-07-03 12:02:20 -0700

    completed saving functionality, conditional styling still in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,97 @@
-
+var calendar = {
+    "0": "",
+    "1": "",
+    "2": "",
+    "3": "",
+    "4": "",
+    "5": "",
+    "6": "",
+    "7": "",
+    "8": "",
+    "9": ""
+}
+var sel = $(`[data-num]`)
+var calEnts = Object.entries(calendar)
+var calVals = Object.values(calendar)
 var saveBtn = $("button")
 var txtArea = $("textarea")
-i=0
-var txtAreaCstm = $(`[data-time = "${i}"]`)
-var calendar= {
-    "0" : "",
-    "1" : "",
-    "2" : "",
-    "3" : "",
-    "4" : "",
-    "5" : "",
-    "6" : "",
-    "7" : "",
-    "8" : "",
-    "9" : ""
-}
 
 //clock on page load
-setInterval(function(){    
+setInterval(function () {
     var cTime = moment().format("MM/DD/YYYY hh:mm:ss");
-    
+
     $("#clock").text(cTime);
 
-},1000);
+}, 1000);
 
-//on page load display val to text area from local storage by data-time=i from calendar.timekey
+//on page load, clears text areas and grabs saved text from local storage
+function initDisplay() {
 
-
-//on button click
-    //save text in text area to object called calendar, keys are times as strings "800": text area text
-    //clear text areas
-    //display calendar object, key "800" displays at 8:00am text area etcs
-
-
-// clear local storage
-//for loop
-//save current txt area of data-time=i to calendar object
-//stringify object and save to local storage
-//clear all text areas
-//display object keys back to text area from local storage by setting values of text areas with data-time=i from calendar.timekey
-    saveBtn.click(function(){
-
-        localStorage.clear();
-
-        for(i=0; i< 10; i++){
-            var strI = i.toString()
-            console.log(strI)
-            calendar.strI =  txtAreaCstm.val()
-            console.log(calendar.strI)
-        };
-
-        localStorage.setItem("calendar", JSON.stringify(calendar));
-
-        for(i=0; i< 10; i++){
-        var strI = i.toString()
-        txtAreaCstm.text("")
-        }
-
-        var displayCal = localStorage.getItem(JSON.parse("calendar"));
-
-        for(i=0; i<10; i++){
-            var strI = i.toString()
-            textAreaCstm.val(displayCal.strI)
+    for (i = 0; i < calEnts.length; i++) {
+        //var strI = i.toString()
+        $(`[data-time = "${i}"]`).text("")
     }
-});
 
+    if (localStorage.getItem("calendar") !== null) {
+        var displayCal = JSON.parse(localStorage.getItem("calendar"));
 
+        for (i = 0; i < Object.values(displayCal).length; i++) {
+            $(`[data-time = ${i}]`).val(displayCal[i])
+        }
+    }
+}
+initDisplay()
 
-//conditional styling
-    //on page load, if the time of a cell has passed, style it grey
-    //else if time of cell is current hour, style it red
-    // 
+//conditional styling loop based on current time
+function colorize() {
+    
+    
+    var comTime = moment().format("H")
+
+    for (i = 0; i < 10; i++) {
+        var cstmObjVal = parseInt(sel[i].attributes[1].nodeValue)
+        
+        console.log($(`[data-time = ${i}]`))
+        console.log(cstmObjVal)
+        var txtAreaArray = Object.entries(txtArea)
+        console.log(txtAreaArray[i])
+        if (cstmObjVal > comTime) {
+            txtAreaArray[i].addClass("future")
+        } else if (cstmObjVal < comTime){
+            txtAreaArray[i].replace("textarea.form-control.present");
+        } else if (cstmObjVal == comTime){
+            txtAreaArray[i].replace("textarea.form-control.present");
+        };
+    }
+}
+colorize()
+
+//when any of the save buttons are clicked
+saveBtn.click(function (event) {
+    event.stopPropagation();
+
+    //clears local storage to prevent doubling of text when it's pulled back out of local storage
+    localStorage.clear();
+
+    //loops through text areas and stores their content to an object that coorsponds with the data-time custom attribute, saves the object to local storage
+    for (i = 0; i < calEnts.length; i++) {
+        var newContent = $(`[data-time = ${i}]`).val()
+        calEnts[i] = [i, newContent]
+        var editedCal = Object.fromEntries(calEnts)
+        localStorage.setItem("calendar", JSON.stringify(editedCal));
+    };
+
+    //clears text area to prevent doubled display of text
+    for (i = 0; i < 10; i++) {
+        $(`[data-time = ${i}]`).text("")
+    }
+
+    //gets calendar item from local storage and turns it back into an obj
+    JSON.parse(localStorage.getItem("calendar"));
+
+    //sets the value of text areas based on data-time attr and the coorsponding obj entry
+    for (i = 0; i < calEnts.length; i++) {
+        $(`[data-time = ${i}]`).val(calEnts[i][1])
+    }
+}
+);
